5941954a5666c9455e4a6bf12d9cf0b4d81a8271
Jang_S <thanyawit.s@netdesigngroup.com>
2022-09-15 17:13:57 +0700

    first Commit

---
+++ Modules/Dteam/Database/Seeders/DteamDatabaseSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Modules\Dteam\Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Eloquent\Model;
+
+class DteamDatabaseSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Model::unguard();
+
+        // $this->call("OthersTableSeeder");
+    }
+}
